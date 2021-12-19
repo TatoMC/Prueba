f1334dcc498be5881d5f40bdc7ead26bca36e811
TatoMC <96274777+TatoMC@users.noreply.github.com>
2021-12-18 21:52:38 -0600

    Subir archivos

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.jobs-gral', { delay: 500 });
+ScrollReveal().reveal('.jobs-banner-one', { delay: 500 });
+ScrollReveal().reveal('.jobs-banner-two', { delay: 500 });
